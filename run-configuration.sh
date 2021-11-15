1df5d6563cd55636a416ac0de02de13ba91e9002
GFrancV <gfrancv@hotmail.com>
2021-11-15 11:29:21 +0000

    Add the wallpaper settings to startup-congig.sh

--- run-configuration.sh
+++ run-configuration.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+
+#Load the username in a local variable
 usrName=$(whoami)
 
 echo "Starting the configuration!"
 echo "$usrName"
+
+#Intall the essential tools
+echo "Installing brightnessctl"
+sudo apt install brightnessctl
+
+echo "Installing feh"
+sudo apt install feh
+
+#Customazing
+
+#Wallpaper configuration
+echo "Setting the wallpaper"
+echo "Creating the folder Images."
+mkdir ~/Images
+echo "Copying the wallpapers to local directory."
+cp images/wallpapers ~/Images
+echo "By default the wallpaper-abstergo was defined."
+
